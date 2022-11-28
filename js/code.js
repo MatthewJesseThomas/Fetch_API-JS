b218cf013bdb2c7e16bc818f30bcba4e5d7ca6ee
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-28 15:22:56 +0200

    'main'

--- js/code.js
+++ js/code.js
@@ -15,12 +15,12 @@
 // .catch(()=> {
 //     console.log("Please try again Later");
 // });
-let data = [8,9,12,1.2,"Jake"];
+let data = [9, 12, "Jake", 8, 1];
 // =============================================================
-// Adding a New Element into an Array use {push()-function} //
+// Adding a New Element into an Array use {push()-method} //
 // data.push(20)
 // =============================================================
-//  To Remove the First Element use {shift()-function} //
+//  To Remove the First Element use {shift()-method} //
 // data.shift()
 // =============================================================
 // data.pop()
@@ -29,7 +29,7 @@ let data = [8,9,12,1.2,"Jake"];
 // =============================================================
 // data.unshift("Matthew")
 // =============================================================
-// Similar to Filter() //
+// Similar to filter()-method //
 // let onlyNumbers = [];
 // for(let k of data) {
 //     if(typeof k != "string")
@@ -40,3 +40,30 @@ let data = [8,9,12,1.2,"Jake"];
 // console.log(data);
 // console.log(data.filter(Number));
 // console.log(data.filter(item => typeof item == "number"));
+// =============================================================
+// Sorting Use sort()-method //
+// let onlyNumbers = data.filter(Number)
+// console.log(onlyNumbers);
+// let sortedValues = onlyNumbers.sort((a,b)=>{
+//     if(a < b)
+//     return -1;
+//     return 1;
+// });
+// let sortedValues = onlyNumbers.sort((a,b)=>(a-b));
+// console.log(sortedValues);
+// =============================================================
+// Slice slice()-method //
+// console.log(sortedValues.slice());
+// let first = data.slice(0, 2);
+// let second = data.slice(3);
+// =============================================================
+//  Spread Operator to Show all Elements of the Array //
+// let finalResults = [...first, ...second];
+// console.log(finalResults);
+// =============================================================
+// Splice splice()-method //
+// splice(start, removeCount, item1, item2...)
+// console.log(data);
+// data.splice(0, 0,"Matthew","Camryn");
+// data.splice(2,1,"Jeff");
+// console.log(data);
